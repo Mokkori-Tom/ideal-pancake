e9d13d7a67670a3d121eba726bad3eb6d9fd9df3
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-02-24 17:43:32 +0900

    run.bat の更新

--- run.bat
+++ run.bat
@@ -7,7 +7,7 @@ tasklist /FI "IMAGENAME eq dkron.exe" | find /I "dkron.exe" >nul
 REM プロセスが見つからなければ新たに起動する
 if errorlevel 1 (
     echo Starting Dkron...
-    start bash_auto.exe dkron agent --server --bootstrap-expect=1
+    bash_auto.exe -c "dkron agent --server --bootstrap-expect=1" > dkron.log 2>&1
 ) else (
     echo Dkron is already running.
 )
