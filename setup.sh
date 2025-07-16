00c78a46d294c4d9151eaee0fc04443b50f554d1
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-17 03:29:27 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export HOME="$PWD"  # 先に必ず設定
-export OPT="$HOME/../../opt"
+export HOME="$PWD/root"  # 先に必ず設定
+export OPT="$PWD/opt"
 # $HOME/.profile
 # ここでシェル起動
 exec sh --login # https://frippery.org/busybox/
