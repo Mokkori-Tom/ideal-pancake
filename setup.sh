0b6a349f2cecabf29211547ddd30c004236afe80
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-07-19 06:38:41 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# busybox -> ash
-export HOME="$PWD/root"  # 先に必ず設定
-export OPT="$PWD/opt"
-# $HOME/.profile
-# ここでシェル起動
-exec sh --login # https://frippery.org/busybox/
-# >busybox64.exe sh setup.sh
