2a60cb668240c11e16d2a67a62c7fbf81033af9e
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-10-31 01:04:12 +0900

    distri-quick.lisp の更新
    #|~|#という技を覚えたので、使ってみた。

--- distri-quick.lisp
+++ distri-quick.lisp
@@ -1,16 +1,21 @@
-; sbclインストールしておく
 ; write-csv.lisp
-; ライブラリごとコピーする
-; 例えば
-; ./  
-; ├── quicklisp/  
-; └── write-csv.lisp
 
+#|
+sbclインストールしておく
+ライブラリごとコピーする
+例えば
+./  
+├── quicklisp/  
+└── write-csv.lisp
+|#
+
+#|
 ; 実行方法
 ; 開発中は --load 対話環境を起動
 ; sbcl --load write-csv.lisp
 ; 配布後は --script 実行して即終了
 ; sbcl --script write-csv.lisp
+|#
 
 ; 外部更新を禁止の場合（オフライン前提）
 ; (setf ql:*update-dist* nil)         
