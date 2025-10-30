4a2ab05a406304707ec6898862f22900ba736a17
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-10-31 00:36:25 +0900

    distri-quick.lisp の更新
    ディレクトリ構成が有る方が、
    意図が分かりやすいよね。

--- distri-quick.lisp
+++ distri-quick.lisp
@@ -1,7 +1,14 @@
-; 外部更新を禁止（オフライン前提）
+; ライブラリごとコピーする
+; 例えば
+; ./  
+; ├── quicklisp/  
+; └── write-csv.lisp
+
+; 外部更新を禁止の場合（オフライン前提）
 ; (setf ql:*update-dist* nil)         
 ; ローカル quicklisp を使用
 (load "./quicklisp/setup.lisp")     
 ; 必要ライブラリをロード
-; (ql:quickload :cl-csv)              
-; 以降にスクリプトを書く
+(ql:quickload :cl-csv)              
+; 以降にスクリプトを書く 
+ 
