0d1c09b95bb34ec5d268e46f9f501a07f173ee37
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-10-31 00:31:04 +0900

    distri-quick.lisp の更新

--- distri-quick.lisp
+++ distri-quick.lisp
@@ -1,7 +1,7 @@
 ; 外部更新を禁止（オフライン前提）
-;(setf ql:*update-dist* nil)         
+; (setf ql:*update-dist* nil)         
 ; ローカル quicklisp を使用
 (load "./quicklisp/setup.lisp")     
 ; 必要ライブラリをロード
-(ql:quickload :cl-csv)              
+; (ql:quickload :cl-csv)              
 ; 以降にスクリプトを書く
