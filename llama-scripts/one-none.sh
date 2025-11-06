d601d377897029537eb765ac5aa95aa562e51833
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-11-06 23:36:59 +0900

    one-none.sh の更新

--- llama-scripts/one-none.sh
+++ llama-scripts/one-none.sh
@@ -1,4 +1,8 @@
-function one-none
-    set dir (realpath (or $argv[1] .))
+function none-one
+    if test (count $argv) -gt 0
+        set dir (realpath $argv[1])
+    else
+        set dir (realpath .)
+    end
     rg --count-matches "<none>" -g "*" $dir | awk -F: '$2 == 1 {print $1}'
 end
