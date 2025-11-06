76d7bdd833cfa95e796fcb531d0ab70d66b6baf7
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-11-06 23:31:46 +0900

    one-none.sh の更新

--- llama-scripts/one-none.sh
+++ llama-scripts/one-none.sh
@@ -1,1 +1,5 @@
-rg --count-matches "<none>" -g "*" ./target_dir | awk -F: '$2 == 1 {print $1}'
+# 検索対象ディレクトリを指定（引数でもOK）
+set dir (realpath (or $argv[1] .))
+
+# <none> を1回だけ含むファイルを検索
+rg --count-matches "<none>" -g "*" $dir | awk -F: '$2 == 1 {print $1}'
