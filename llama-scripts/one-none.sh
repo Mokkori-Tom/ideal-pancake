d7b8856e52e0096ce56216301917f28616f650d1
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-11-08 00:01:13 +0900

    one-none.sh の更新

--- llama-scripts/one-none.sh
+++ llama-scripts/one-none.sh
@@ -1,8 +1,24 @@
 function one-none
-    if test (count $argv) -gt 0
-        set dir (realpath $argv[1])
+    # 第1引数: 探索ディレクトリ (省略時はカレント)
+    if test (count $argv) -ge 1
+        set src (realpath $argv[1])
     else
-        set dir (realpath .)
+        set src (realpath .)
     end
-    rg --count-matches "<none>" -g "*" $dir | awk -F: '$2 == 1 {print $1}'
+
+    # 第2引数: 保存先ディレクトリ (省略時は ./one-none-out)
+    set dst "./one-none-out"
+    if test (count $argv) -ge 2
+        set dst $argv[2]
+    end
+
+    # 保存先ディレクトリを作成
+    mkdir -p "$dst"
+
+    # <none> がちょうど1回だけ出現するファイルをコピー
+    rg --count-matches "<none>" -g "*" "$src" \
+    | awk -F: '$2 == 1 {print $1}' \
+    | while read -l f
+        cp -- "$f" "$dst"/
+    end
 end
