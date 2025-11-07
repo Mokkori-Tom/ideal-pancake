edbd15cdcd9e5687c3586379636998102b0bf826
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-11-08 00:11:25 +0900

    clean-prompts.fish の更新

--- llama-scripts/clean-prompts.fish
+++ llama-scripts/clean-prompts.fish
@@ -1,24 +1,34 @@
 function clean-prompts
-    # 使い方:
-    # clean-prompts [入力ディレクトリ] [出力ディレクトリ]
-    # 例: clean-prompts ./in ./out
+    # 第1引数: 入力ディレクトリ (省略時はカレント)
+    if test (count $argv) -ge 1
+        set indir (realpath $argv[1])
+    else
+        set indir (realpath .)
+    end
 
-    set indir (or $argv[1] ./in)
-    set outdir (or $argv[2] ./out)
+    # 第2引数: 出力ディレクトリ (省略時は ./clean-prompts-out)
+    set outdir "./clean-prompts-out"
+    if test (count $argv) -ge 2
+        set outdir $argv[2]
+    end
 
-    mkdir -p $outdir
+    # 出力ディレクトリ作成（ルート直下にならないよう realpath は使わない）
+    mkdir -p "$outdir"
 
-    # ファイルを再帰的に処理
-    for f in (find $indir -type f)
-        set rel (string replace -r "^$indir/" "" $f)
+    # 再帰的にファイル処理（ディレクトリ構造を保持）
+    find "$indir" -type f | while read -l f
+        # indir/ 以降の相対パスを取り出し
+        set rel (string replace -r "^$indir/" "" -- "$f")
         set out "$outdir/$rel"
-        mkdir -p (dirname $out)
 
-        # タグ除去処理
+        # 必要なディレクトリを作成
+        mkdir -p (dirname "$out")
+
+        # タグ削除処理
         perl -0pe '
             s/\A.*?<\/sys-prompt>//s;
             s/\A.*?<\/main-prompt>//s;
             s/<think>.*?<\/think>//sg;
-        ' $f > $out
+        ' "$f" > "$out"
     end
 end
