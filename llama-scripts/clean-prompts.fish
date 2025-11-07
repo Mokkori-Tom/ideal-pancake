286a827023dc63db12640bc3f5f9ec5d526c72df
Mokkori-Tom <184056355+Mokkori-Tom@users.noreply.github.com>
2025-11-08 00:08:06 +0900

    clean-prompts.fish の更新

--- llama-scripts/clean-prompts.fish
+++ llama-scripts/clean-prompts.fish
@@ -3,18 +3,18 @@ function clean-prompts
     # clean-prompts [入力ディレクトリ] [出力ディレクトリ]
     # 例: clean-prompts ./in ./out
 
-    set indir (realpath (or $argv[1] ./in))
-    set outdir (realpath (or $argv[2] ./out))
+    set indir (or $argv[1] ./in)
+    set outdir (or $argv[2] ./out)
 
     mkdir -p $outdir
 
-    # 再帰的にファイルを処理
+    # ファイルを再帰的に処理
     for f in (find $indir -type f)
         set rel (string replace -r "^$indir/" "" $f)
         set out "$outdir/$rel"
         mkdir -p (dirname $out)
 
-        # 変換処理: タグ削除
+        # タグ除去処理
         perl -0pe '
             s/\A.*?<\/sys-prompt>//s;
             s/\A.*?<\/main-prompt>//s;
